fdac59398c9893b4269d5302ae2410061e4751dc
Abhishek Bose <abhishekbose87@gmail.com>
2020-10-17 16:52:15 +0530

    Setup tailwind CSS

---
+++ pages/support.js
@@ -0,0 +1,13 @@
+export default function Support() {
+  return (
+    <div className="bg-white rounded-lg p-24 justify-center">
+      <img
+        className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+        src="https://avatars.slack-edge.com/2019-10-07/788234891670_bd6f0917891d2f12834e_192.jpg"
+      />
+      <div className="text-center md:text-left">
+        <h2 className="text-lg text-blue-400">Saurabh</h2>
+      </div>
+    </div>
+  );
+}
