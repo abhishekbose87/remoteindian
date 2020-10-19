dc9b4134fd64c93178b53638734c033530f7ac8d
Abhishek Bose <abhishekbose87@gmail.com>
2020-10-19 19:57:47 +0530

    Add heroicons

--- components/supporter/index.js
+++ components/supporter/index.js
@@ -14,7 +14,19 @@ const SupporterSection = ({title, file}) => {
               <div class="font-bold my-2 px-2 text-center text-gray-900">
                 {supporter.name}
               </div>
-              <p class="text-gray-700 text-base text-center self-end">{supporter.url}</p>
+              <div class="mx-auto self-end mb-2">
+                <a href={supporter.url}>
+                  <svg
+                    class="fill-current w-4 h-4 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                  </svg>
+                </a>
+              </div>
             </div>
           ))}
         </div>
--- pages/support.js
+++ pages/support.js
@@ -20,14 +20,29 @@ export default function Support() {
         <h2 class="">🌟 Sponsors</h2>
         <div class="grid md:grid-cols-2 items-stretch">
           <img src="https://via.placeholder.com/552x270" />
-          <div class="grid md:text-left bg-gray-100 px-5 py-2">
+          <div class="grid sm:text-center md:text-left bg-gray-100 px-5 py-2 items-center">
             <h3>ScaleReal Technologies</h3>
             <p>
               ScaleReal is a team of like minded people building high quality
               products. They believe in Open, Remote and Agile work culture
               apart from Technology, People, Process and Strategy.
             </p>
-            <div class="self-end">Icon</div>
+            <div class="self-end mb-2 sm:mx-auto md:mx-0 ">
+              <a href="#">
+                <svg
+                  class="fill-current w-6 h-6 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
